e8e523e1ada7392b3b1673f8954a6df5fb567e95
pierreyoda <pierreyoda@users.noreply.github.com>
2025-12-26 19:59:30 +0100

    :sparkles: Day 1 Part 2 done

--- src/day-1/main.rs
+++ src/day-1/main.rs
@@ -67,32 +67,75 @@ impl VaultDial {
         }
     }
 
+    pub fn reset(&mut self, to_value: Option<VaultDialScalar>) {
+        self.value = to_value.unwrap_or(VAULT_DIAL_INITIAL_VALUE);
+    }
+
     pub fn get_value(&self) -> VaultDialScalar {
         self.value
     }
 
-    pub fn solve(&mut self, rotations: &[VaultDialRotation]) -> i32 {
+    pub fn solve(&mut self, rotations: &[VaultDialRotation], part: &DayPuzzlePart) -> i32 {
         let mut dial_zero_values = 0;
         for dial_rotation in rotations {
-            self.rotate(dial_rotation);
-            if self.value == 0 {
-                dial_zero_values += 1;
+            match part {
+                DayPuzzlePart::One => {
+                    self.rotate(dial_rotation);
+                    if self.value == 0 {
+                        dial_zero_values += 1;
+                    }
+                }
+                DayPuzzlePart::Two => dial_zero_values += self.rotate_part_2(dial_rotation),
             }
         }
         dial_zero_values
     }
 
+    /// Rotates the vault dial according to the given rotation.
     pub fn rotate(&mut self, rotation: &VaultDialRotation) {
         match rotation.direction {
             VaultDialRotationDirection::Left => {
-                self.value = ((self.value + VAULT_DIAL_ROTATION_RANGE
+                self.value = (self.value + VAULT_DIAL_ROTATION_RANGE
                     - (rotation.distance % VAULT_DIAL_ROTATION_RANGE))
-                    % VAULT_DIAL_ROTATION_RANGE);
+                    % VAULT_DIAL_ROTATION_RANGE;
             }
             VaultDialRotationDirection::Right => {
                 self.value = (self.value + rotation.distance) % (VAULT_DIAL_MAX_VALUE + 1);
             }
         }
+    }
+
+    /// Rotates the vault dial according to the given rotation.
+    ///
+    /// Returns the number of times the dial pointed at 0 during the rotation.
+    pub fn rotate_part_2(&mut self, rotation: &VaultDialRotation) -> i32 {
+        let mut zero_value_passes = 0;
+        let mut incrementally_rotate = |direction: &VaultDialRotationDirection| {
+            self.value = match direction {
+                VaultDialRotationDirection::Left => {
+                    if self.value == 0 {
+                        VAULT_DIAL_MAX_VALUE
+                    } else {
+                        self.value - 1
+                    }
+                }
+                VaultDialRotationDirection::Right => {
+                    if self.value == VAULT_DIAL_MAX_VALUE {
+                        0
+                    } else {
+                        self.value + 1
+                    }
+                }
+            };
+            if self.value == 0 {
+                zero_value_passes += 1;
+            }
+        };
+
+        for _ in 0..rotation.distance {
+            incrementally_rotate(&rotation.direction);
+        }
+        zero_value_passes
     }
 }
 
@@ -104,7 +147,18 @@ fn main() -> Result<()> {
         b'\n',
         |input: Vec<VaultDialRotation>| {
             let mut dial = VaultDial::new(None);
-            Ok(dial.solve(&input))
+            Ok(dial.solve(&input, &DayPuzzlePart::One))
+        },
+    )?;
+
+    // Part 2
+    run_day_puzzle_solver(
+        1,
+        DayPuzzlePart::Two,
+        b'\n',
+        |input: Vec<VaultDialRotation>| {
+            let mut dial = VaultDial::new(None);
+            Ok(dial.solve(&input, &DayPuzzlePart::Two))
         },
     )?;
 
@@ -113,7 +167,24 @@ fn main() -> Result<()> {
 
 #[cfg(test)]
 mod tests {
+    use advent_2025_common::DayPuzzlePart;
+
     use crate::{VaultDial, VaultDialRotation, VaultDialRotationDirection};
+
+    fn get_testing_rotations() -> Vec<VaultDialRotation> {
+        vec![
+            VaultDialRotation::try_from("L68".to_string()).unwrap(),
+            VaultDialRotation::try_from("L30".to_string()).unwrap(),
+            VaultDialRotation::try_from("R48".to_string()).unwrap(),
+            VaultDialRotation::try_from("L5".to_string()).unwrap(),
+            VaultDialRotation::try_from("R60".to_string()).unwrap(),
+            VaultDialRotation::try_from("L55".to_string()).unwrap(),
+            VaultDialRotation::try_from("L1".to_string()).unwrap(),
+            VaultDialRotation::try_from("L99".to_string()).unwrap(),
+            VaultDialRotation::try_from("R14".to_string()).unwrap(),
+            VaultDialRotation::try_from("L82".to_string()).unwrap(),
+        ]
+    }
 
     #[test]
     fn test_day_1_vault_dial_rotation() {
@@ -144,19 +215,15 @@ mod tests {
 
     #[test]
     fn test_day_1_vault_dial_solving() {
-        let rotations = vec![
-            VaultDialRotation::try_from("L68".to_string()).unwrap(),
-            VaultDialRotation::try_from("L30".to_string()).unwrap(),
-            VaultDialRotation::try_from("R48".to_string()).unwrap(),
-            VaultDialRotation::try_from("L5".to_string()).unwrap(),
-            VaultDialRotation::try_from("R60".to_string()).unwrap(),
-            VaultDialRotation::try_from("L55".to_string()).unwrap(),
-            VaultDialRotation::try_from("L1".to_string()).unwrap(),
-            VaultDialRotation::try_from("L99".to_string()).unwrap(),
-            VaultDialRotation::try_from("R14".to_string()).unwrap(),
-            VaultDialRotation::try_from("L82".to_string()).unwrap(),
-        ];
+        let rotations = get_testing_rotations();
         let mut vault_dial = VaultDial::new(None);
-        assert_eq!(vault_dial.solve(&rotations), 3);
+        assert_eq!(vault_dial.solve(&rotations, &DayPuzzlePart::One), 3);
+    }
+
+    #[test]
+    fn test_day_1_part_2_vault_dial_solving() {
+        let rotations = get_testing_rotations();
+        let mut vault_dial = VaultDial::new(None);
+        assert_eq!(vault_dial.solve(&rotations, &DayPuzzlePart::Two), 6);
     }
 }
